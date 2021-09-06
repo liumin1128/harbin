5787bd821f3698d6651aba1654cea6dcad9993bf
liumin <970568830@qq.com>
2021-09-06 23:55:44 +0800

    feat: 更新基础项目结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { resolve } from 'path';
-import handlebars from 'vite-plugin-handlebars';
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
 
 const pageData = {
-  '/index.html': {
-    title: 'Main Page',
+  "/index.html": {
+    title: "哈尔滨新区",
+    navList: [
+      { title: "新区发布", url: "#" },
+      { title: "双创惠", url: "#" },
+      { title: "活动惠", url: "#" },
+      { title: "政策惠", url: "#" },
+      { title: "场地惠", url: "#" },
+      { title: "资本惠", url: "#" },
+      { title: "服务惠", url: "#" },
+      { title: "项目惠", url: "#" },
+    ],
   },
-  '/nested/subpage.html': {
-    title: 'Sub Page',
+  "/nested/subpage.html": {
+    title: "Sub Page",
   },
 };
 
@@ -16,7 +26,7 @@ export default {
       context(pagePath) {
         return pageData[pagePath];
       },
-      partialDirectory: resolve('./src', 'partials'),
+      partialDirectory: resolve("./src", "partials"),
     }),
   ],
 };

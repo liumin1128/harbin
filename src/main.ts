3432ecf0c17481bfe51e8505c0868ade973ecaed
liumin <970568830@qq.com>
2021-09-14 08:30:53 +0800

    feat: 基础的移动端适配

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import "jquery";
 import "bootstrap";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./theme.scss";
-import "./common.scss";
+// import "./theme.scss";
+// import "./common.scss";
 import "./index.scss";

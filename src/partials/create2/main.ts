a8678cb19e73a0770f9faf3100450664fa6515e6
liumin <970568830@qq.com>
2021-10-09 06:13:12 +0800

    feat: 多项输入

--- src/partials/create2/main.ts
+++ src/partials/create2/main.ts
@@ -1,23 +1,25 @@
 import $ from "jquery";
 
-// @ts-ignore
-// var forms = document.querySelectorAll(".create-form-step2");
-document.querySelector(".create-form-step2").addEventListener(
-  // Array.prototype.slice.call(forms).forEach(function (form) {
-  // form.addEventListener(
-  "submit",
-  function (event: any) {
-    // 阻止浏览器默认提交事件，如果不需要就移除下面这两句
-    event.preventDefault();
-    event.stopPropagation();
+setTimeout(() => {
+  // @ts-ignore
+  // var forms = document.querySelectorAll(".create-form-step2");
+  document.querySelector(".create-form-step2").addEventListener(
+    // Array.prototype.slice.call(forms).forEach(function (form) {
+    // form.addEventListener(
+    "submit",
+    function (event: any) {
+      // 阻止浏览器默认提交事件，如果不需要就移除下面这两句
+      event.preventDefault();
+      event.stopPropagation();
 
-    // get form 表单的值，用于构建自定义请求
-    let json = $(".create-form-step2").serialize();
-    console.log("json : ", json);
-    alert(JSON.stringify(json));
+      // get form 表单的值，用于构建自定义请求
+      let json = $(".create-form-step2").serialize();
+      console.log("json : ", json);
+      alert(JSON.stringify(json));
 
-    window.location.pathname = "/create3.html";
-  },
-  false
-);
-// });
+      window.location.pathname = "/create3.html";
+    },
+    false
+  );
+  // });
+}, 300);
